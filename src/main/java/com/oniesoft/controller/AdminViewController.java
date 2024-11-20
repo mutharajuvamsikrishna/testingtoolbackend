200a2fad50247e53b4bd18fa8eeed6108d21b819
mutharajuvamsikrishna <130433935+mutharajuvamsikrishna@users.noreply.github.com>
2024-11-20 11:18:17 +0530

    changes on adminview

--- src/main/java/com/oniesoft/controller/AdminViewController.java
+++ src/main/java/com/oniesoft/controller/AdminViewController.java
@@ -6,11 +6,13 @@ import com.oniesoft.service.AdminViewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/adminview/v1")
 public class AdminViewController {
     @Autowired
     private AdminViewService adminViewService;
@@ -18,7 +20,7 @@ public class AdminViewController {
     public List<Branch> findBranchsByCmpId(@PathVariable int cmpId){
         return adminViewService.findBranchByCmpId(cmpId);
     }
-    @GetMapping
+    @GetMapping("/registersbybranchId/{branchId}")
     public List<Register> findRegistersByBranchId(@PathVariable int branchId){
         return adminViewService.findRegisterByBranchId(branchId);
     }
